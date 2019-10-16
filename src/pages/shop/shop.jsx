6aab84c5d16bae627b5e82cdfc1b9005fd24fa19
leighd2008 <leighd2008@gmail.com>
2019-10-16 07:30:30 -0400

    adjusted logic for isLoading to match changed due to moving async get function into redux

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -7,7 +7,10 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 import CollectionPage from "../collection/collection";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionFetchng } from "../../redux/shop/shop.selectors";
+import {
+  selectIsCollectionFetchng,
+  selectIsCollectionsLoaded
+} from "../../redux/shop/shop.selectors";
 
 import WithSpinner from "../../components/with-spinner/with-spinner";
 
@@ -21,7 +24,7 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionFetching } = this.props;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
     return (
       <div className="shop-page">
         <Route
@@ -38,7 +41,7 @@ class ShopPage extends React.Component {
           path={`${match.path}/:collectionId`}
           render={props => (
             <CollectionPageWithSpinner
-              isLoading={isCollectionFetching}
+              isLoading={!isCollectionsLoaded}
               {...props}
             />
           )}
@@ -49,7 +52,8 @@ class ShopPage extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetchng
+  isCollectionFetching: selectIsCollectionFetchng,
+  isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
