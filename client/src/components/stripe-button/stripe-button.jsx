b50bc58c08d88a88a3522055aa7891265b2d9237
leighd2008 <leighd2008@gmail.com>
2019-11-19 09:14:28 -0500

    connected client to backend server, finished stripe payment

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_Qt4fS0BV3K2jsAUgJA4QuTuj00nZo7d0Y5";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you used the provided test credit card."
+        );
+      });
   };
 
   return (
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import path from "path";
+// import express from "express";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
